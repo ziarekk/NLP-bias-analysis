a1520d4b48a92b482b7b5efb581bb868be38ee9e
ziarekk <01169264@pw.edu.pl>
2024-05-18 18:00:07 +0200

    Add bias measuring pipelines for bold and honest

--- toxicity_pipeline.py
+++ toxicity_pipeline.py
@@ -69,30 +69,120 @@ def wino_bias_dataset_inference(text_generation, metric):
     return scores
 
 
+def bold_dataset_inference(text_generation, metric):
+    bold_dataset = load_dataset("AlexaAI/bold", split="train")
+    
+    # on the entier dataset
+    filtered_prompts = bold_dataset.filter(lambda subset: subset['domain'] == 'gender')
+    
+    # on a sampled subeset with equal ratio of male and female prompts
+    # female_bold = (random.sample([p for p in bold_dataset if p['category'] == 'American_actresses'],4))
+    # male_bold = (random.sample([p for p in bold_dataset if p['category'] == 'American_actors'],4))
+    # filtered_prompts = female_bold + male_bold
+    
+    female_continuations = []
+    male_continuations = []
+
+    for i, prompt in tqdm(enumerate(filtered_prompts)):
+        sample = prompt['prompts'][0]
+        generation = text_generation(sample, max_length=50, do_sample=False, pad_token_id=50256)
+        continuation = generation[0]['generated_text'].replace(sample,'')
+        
+        if prompt['category'] == 'American_actresses':
+            female_continuations.append(continuation)
+        elif prompt['category'] == 'American_actors':
+            male_continuations.append(continuation)
+        
+        print(f'bold, e:{i}')    
+        if i == 7:
+            break
+        
+    scores = metric.compute(data = male_continuations, references= female_continuations, aggregation = 'average')
+    
+    scores['male_scores'] = scores.pop('average_data_regard')
+    scores['female_scores'] = scores.pop('average_references_regard')
+    
+    return scores
+    
+
+def honest_dataset_inference(text_generation, tokenizer, metric):
+    honest_dataset = load_dataset("MilaNLProc/honest", 'en_binary', split='honest')
+    
+    
+    # on the entier dataset
+    prompts = [(h['template_masked'].replace(' [M].',''), h['category']) for h in honest_dataset]
+    
+    # on a sampled subeset with equal ratio of male and female prompts
+    # female_prompts = random.sample([(h['template_masked'].replace(' [M].',''), h['category']) for h in honest_dataset if h['category'].startswith('female')], 40)
+    # male_prompts = random.sample([(h['template_masked'].replace(' [M].',''), h['category']) for h in honest_dataset if h['category'].startswith('male')], 40)
+    # prompts = female_prompts + male_prompts
+   
+    #num of generations
+    k = 20
+    male_continuations = []
+    female_continuations = []
+    
+    for i, prompt in tqdm(enumerate(prompts)):
+        sample = prompt[0]      # take prompt text - 'template_masked'
+        
+        generation = text_generation(sample, max_length=len(tokenizer(sample)['input_ids'])+10, num_return_sequences=k, pad_token_id=50256)
+        continuation = generation[0]['generated_text'].replace(sample,'')
+        
+        # check 'category'
+        if prompt[1].startswith('male'):    
+            male_continuations.append(continuation)
+        elif prompt[1].startswith('female'):
+            female_continuations.append(continuation)
+        
+        if i == 79:
+            break
+
+    female_continuations = [continuation.split() for continuation in female_continuations]
+    male_continuations = [continuation.split() for continuation in male_continuations]
+    
+    scores = {
+        "male_avg_regard": metric.compute(predictions=male_continuations),
+        "female_avg_regard": metric.compute(predictions=female_continuations)
+    }
+
+    return scores
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', choices=["gpt2", "gemma", "phi2"], required=True)
-    parser.add_argument('--dataset', choices=["toxicity", "wino_bias"], required=True)
+    parser.add_argument('--dataset', choices=["toxicity", "wino_bias", "bold", "honest"], required=True)
+    
     args = parser.parse_args()
-
+    
     # Load model
     text_generation, tokenizer = get_model(args.model)
-    toxicity = evaluate.load("toxicity")
+    toxicity = evaluate.load('toxicity')
+    regard = evaluate.load('regard', 'compare')
+    honest = evaluate.load('honest', 'en')
 
     # Inference
     if args.dataset == "toxicity":
         scores = toxicity_dataset_inference(text_generation, toxicity)
+        metric = "toxicity"
     elif args.dataset == "wino_bias":
         scores = wino_bias_dataset_inference(text_generation, toxicity)
+        metric = "toxicity"
+    elif args.dataset == "bold":
+        scores = bold_dataset_inference(text_generation, regard)
+        metric = "regard"
+    elif args.dataset == "honest":
+        scores = honest_dataset_inference(text_generation, tokenizer, honest)
+        metric = "honest"
+
 
     result_dict = {
         "model": args.model,
         "dataset": args.dataset,
-        "metric": "toxicity",
+        "metric": metric,
         "scores": scores,
     }
 
-    path = f"results/toxicity_{args.model}_{args.dataset}.json"
+    path = f"results/{metric}_{args.model}_{args.dataset}.json"
     with open(path, 'w') as fp:
         json.dump(result_dict, fp, indent=4)
 
